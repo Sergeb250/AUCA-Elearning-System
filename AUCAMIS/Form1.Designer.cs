3adcb34ae4ca950c30f5cbe85eb3aea2eae5e9ee
Serge Benit <hacksergeb@gmail.com>
2025-09-17 22:24:07 +0200

    Add project files.

---
+++ AUCAMIS/Form1.Designer.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace AUCAMIS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.txtCourseCode = new System.Windows.Forms.TextBox();
+            this.txtInstructor = new System.Windows.Forms.TextBox();
+            this.txtSemester = new System.Windows.Forms.TextBox();
+            this.txtCourseName = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.CreateBtn = new System.Windows.Forms.Button();
+            this.updateBtn = new System.Windows.Forms.Button();
+            this.deleteBtn = new System.Windows.Forms.Button();
+            this.resetBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.dataTablePanel = new System.Windows.Forms.TableLayoutPanel();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(344, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 30);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Instructor";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 133);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 30);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Semester";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 87);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(143, 30);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "CourseName";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(344, 133);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(135, 30);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "CourseCode";
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Font = new System.Drawing.Font("Segoe UI Historic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDescription.Location = new System.Drawing.Point(174, 177);
+            this.txtDescription.Multiline = true;
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(484, 86);
+            this.txtDescription.TabIndex = 7;
+            // 
+            // txtCourseCode
+            // 
+            this.txtCourseCode.Font = new System.Drawing.Font("Segoe UI Historic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCourseCode.Location = new System.Drawing.Point(499, 133);
+            this.txtCourseCode.Multiline = true;
+            this.txtCourseCode.Name = "txtCourseCode";
+            this.txtCourseCode.Size = new System.Drawing.Size(227, 38);
+            this.txtCourseCode.TabIndex = 8;
+            // 
+            // txtInstructor
+            // 
+            this.txtInstructor.Font = new System.Drawing.Font("Segoe UI Historic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInstructor.Location = new System.Drawing.Point(499, 74);
+            this.txtInstructor.Multiline = true;
+            this.txtInstructor.Name = "txtInstructor";
+            this.txtInstructor.Size = new System.Drawing.Size(227, 54);
+            this.txtInstructor.TabIndex = 9;
+            // 
+            // txtSemester
+            // 
+            this.txtSemester.Font = new System.Drawing.Font("Segoe UI Historic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSemester.Location = new System.Drawing.Point(153, 123);
+            this.txtSemester.Multiline = true;
+            this.txtSemester.Name = "txtSemester";
+            this.txtSemester.Size = new System.Drawing.Size(173, 48);
+            this.txtSemester.TabIndex = 10;
+            // 
+            // txtCourseName
+            // 
+            this.txtCourseName.Font = new System.Drawing.Font("Segoe UI Historic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCourseName.Location = new System.Drawing.Point(153, 74);
+            this.txtCourseName.Multiline = true;
+            this.txtCourseName.Name = "txtCourseName";
+            this.txtCourseName.Size = new System.Drawing.Size(173, 43);
+            this.txtCourseName.TabIndex = 11;
+            this.txtCourseName.Text = "S";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 177);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(129, 30);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Description";
+            // 
+            // CreateBtn
+            // 
+            this.CreateBtn.Location = new System.Drawing.Point(174, 263);
+            this.CreateBtn.Name = "CreateBtn";
+            this.CreateBtn.Size = new System.Drawing.Size(104, 43);
+            this.CreateBtn.TabIndex = 13;
+            this.CreateBtn.Text = "CREATE";
+            this.CreateBtn.UseVisualStyleBackColor = true;
+            this.CreateBtn.Click += new System.EventHandler(this.CreateBtn_Click);
+            // 
+            // updateBtn
+            // 
+            this.updateBtn.Location = new System.Drawing.Point(284, 267);
+            this.updateBtn.Name = "updateBtn";
+            this.updateBtn.Size = new System.Drawing.Size(123, 39);
+            this.updateBtn.TabIndex = 14;
+            this.updateBtn.Text = "UPDATE";
+            this.updateBtn.UseVisualStyleBackColor = true;
+            this.updateBtn.Click += new System.EventHandler(this.updateBtn_Click);
+            // 
+            // deleteBtn
+            // 
+            this.deleteBtn.Location = new System.Drawing.Point(413, 267);
+            this.deleteBtn.Name = "deleteBtn";
+            this.deleteBtn.Size = new System.Drawing.Size(117, 41);
+            this.deleteBtn.TabIndex = 15;
+            this.deleteBtn.Text = "DELETE";
+            this.deleteBtn.UseVisualStyleBackColor = true;
+            this.deleteBtn.Click += new System.EventHandler(this.deleteBtn_Click);
+            // 
+            // resetBtn
+            // 
+            this.resetBtn.Location = new System.Drawing.Point(544, 269);
+            this.resetBtn.Name = "resetBtn";
+            this.resetBtn.Size = new System.Drawing.Size(114, 39);
+            this.resetBtn.TabIndex = 16;
+            this.resetBtn.Text = "RESET";
+            this.resetBtn.UseVisualStyleBackColor = true;
+            this.resetBtn.Click += new System.EventHandler(this.resetBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Historic", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(231, 26);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(342, 45);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "AUCA E-learn System";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(214, 392);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 30);
+            this.label8.TabIndex = 17;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(309, 309);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(208, 30);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Registered Courses";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // dataTablePanel
+            // 
+            this.dataTablePanel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dataTablePanel.ColumnCount = 6;
+            this.dataTablePanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 26.75159F));
+            this.dataTablePanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 73.24841F));
+            this.dataTablePanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 109F));
+            this.dataTablePanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 132F));
+            this.dataTablePanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 117F));
+            this.dataTablePanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 221F));
+            this.dataTablePanel.Font = new System.Drawing.Font("Segoe UI Historic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dataTablePanel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dataTablePanel.Location = new System.Drawing.Point(755, 378);
+            this.dataTablePanel.Name = "dataTablePanel";
+            this.dataTablePanel.RowCount = 6;
+            this.dataTablePanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 37F));
+            this.dataTablePanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 31F));
+            this.dataTablePanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.dataTablePanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 39F));
+            this.dataTablePanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.dataTablePanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 8F));
+            this.dataTablePanel.Size = new System.Drawing.Size(49, 29);
+            this.dataTablePanel.TabIndex = 19;
+            this.dataTablePanel.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(42, 354);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(707, 359);
+            this.dataGridView2.TabIndex = 28;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(944, 749);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataTablePanel);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.resetBtn);
+            this.Controls.Add(this.deleteBtn);
+            this.Controls.Add(this.updateBtn);
+            this.Controls.Add(this.CreateBtn);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtCourseName);
+            this.Controls.Add(this.txtSemester);
+            this.Controls.Add(this.txtInstructor);
+            this.Controls.Add(this.txtCourseCode);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI Historic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.Name = "Form1";
+            this.Text = "AUCA MIS";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+            // You can leave this empty or add custom painting logic if needed
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.TextBox txtCourseCode;
+        private System.Windows.Forms.TextBox txtInstructor;
+        private System.Windows.Forms.TextBox txtSemester;
+        private System.Windows.Forms.TextBox txtCourseName;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button CreateBtn;
+        private System.Windows.Forms.Button updateBtn;
+        private System.Windows.Forms.Button deleteBtn;
+        private System.Windows.Forms.Button resetBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TableLayoutPanel dataTablePanel;
+        private DataGridView dataGridView2;
+    }
+}
+
---
+++ AUCAMIS/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace AUCAMIS
+{
+    public partial class Form1 : Form
+    {
+        private int courseCounter = 0;
+        private DataTable dataTable = new DataTable();
+        private DataGridView dataGridView1;
+        private Button createBtn;
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Add this line to assign the existing DataGridView from the designer
+            dataGridView1 = this.Controls.Find("dataGridView2", true).FirstOrDefault() as DataGridView;
+            // Add this line to assign the existing Button from the designer
+            createBtn = this.Controls.Find("createBtn", true).FirstOrDefault() as Button;
+            if (dataGridView1 != null)
+            {
+                dataGridView1.CellClick += dataGridView1_CellClick;
+            }
+            this.Load += new System.EventHandler(this.Form1_Load);
+
+            // Disable the create button initially
+            if (createBtn != null)
+                createBtn.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Setup DataTable
+            dataTable.Columns.Add("ID", typeof(string));
+            dataTable.Columns.Add("Course Name", typeof(string));
+            dataTable.Columns.Add("Course Code", typeof(string));
+            dataTable.Columns.Add("Instructor", typeof(string));
+            dataTable.Columns.Add("Semester", typeof(string));
+            dataTable.Columns.Add("Description", typeof(string));
+
+            // Bind to grid
+            dataGridView1.DataSource = dataTable;
+
+            // Enable the create button after loading is complete
+            createBtn.Enabled = true;
+        }
+
+        // CREATE
+        private void CreateBtn_Click(object sender, EventArgs e)
+        {
+            string courseName = txtCourseName.Text.Trim();
+            string courseCode = txtCourseCode.Text.Trim();
+            string instructor = txtInstructor.Text.Trim();
+            string semester = txtSemester.Text.Trim();
+            string description = txtDescription.Text.Trim();
+
+            if (string.IsNullOrEmpty(courseName) ||
+                string.IsNullOrEmpty(courseCode) ||
+                string.IsNullOrEmpty(instructor) ||
+                string.IsNullOrEmpty(semester))
+            {
+                MessageBox.Show("All required fields must be filled.");
+                return;
+            }
+
+            if (!Regex.IsMatch(courseCode, @"^[A-Za-z0-9]+$"))
+            {
+                MessageBox.Show("Invalid Course Code.");
+                return;
+            }
+
+            // Generate ID
+            string id = "AUCA" + (courseCounter + 1).ToString("D2");
+
+            // Add row
+            dataTable.Rows.Add(id, courseName, courseCode, instructor, semester, description);
+            courseCounter++;
+
+            ClearFields();
+            MessageBox.Show("Course added.");
+        }
+
+        // UPDATE
+        private void updateBtn_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow == null) return;
+
+            DataGridViewRow row = dataGridView1.CurrentRow;
+            row.Cells["Course Name"].Value = txtCourseName.Text.Trim();
+            row.Cells["Course Code"].Value = txtCourseCode.Text.Trim();
+            row.Cells["Instructor"].Value = txtInstructor.Text.Trim();
+            row.Cells["Semester"].Value = txtSemester.Text.Trim();
+            row.Cells["Description"].Value = txtDescription.Text.Trim();
+
+            ClearFields();
+            MessageBox.Show("Course updated.");
+        }
+
+        // DELETE
+        private void deleteBtn_Click(object sender, EventArgs e)
+        {
+            string code = txtCourseCode.Text.Trim();
+            if (string.IsNullOrEmpty(code)) return;
+
+            DataRow row = dataTable.AsEnumerable()
+                                   .FirstOrDefault(r => r.Field<string>("Course Code")
+                                   .Equals(code, StringComparison.OrdinalIgnoreCase));
+
+            if (row != null)
+            {
+                dataTable.Rows.Remove(row);
+                ClearFields();
+                MessageBox.Show("Course deleted.");
+            }
+            else
+            {
+                MessageBox.Show("Course not found.");
+            }
+        }
+
+        // RESET
+        private void resetBtn_Click(object sender, EventArgs e)
+        {
+            ClearFields();
+            dataGridView1.ClearSelection();
+        }
+
+        // Fill fields when row clicked
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0) return;
+
+            DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+            txtCourseName.Text = row.Cells["Course Name"].Value.ToString();
+            txtCourseCode.Text = row.Cells["Course Code"].Value.ToString();
+            txtInstructor.Text = row.Cells["Instructor"].Value.ToString();
+            txtSemester.Text = row.Cells["Semester"].Value.ToString();
+            txtDescription.Text = row.Cells["Description"].Value.ToString();
+        }
+
+        // Helper
+        private void ClearFields()
+        {
+            txtCourseName.Clear();
+            txtCourseCode.Clear();
+            txtInstructor.Clear();
+            txtSemester.Clear();
+            txtDescription.Clear();
+        }
+
+        
+    }
+}
+
+
+
